cd0194413b0875baeff54b4cf345a0edc21f73f3
mimi klein <lifeistoogoodtobetrue@gmail.com>
2017-02-27 18:24:19 -0500

    change file path for heroku deployment

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ app.use(express.static(path.join(__dirname, 'client/build')));
 // })
 // Always return the main index.html, so react-router can render the route in the client
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'client/build', 'index.html'));
+  res.sendFile(path.resolve(__dirname, 'client/build', '../public/index.html'));
 });
 
 
